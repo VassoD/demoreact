aa191eb7acd2d17af9347c44609b70b228be7321
vassod <vassodor@gmail.com>
2021-05-11 18:22:55 +0300

    className

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+import MetricItem from "./components/MetricItem"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hello Odyssea</h1>
+      <p>Hi from inside</p>
+      <MetricItem />
+      </div>
   );
 }
-
+ 
 export default App;
--- src/components/metricitem.js
+++ src/components/metricitem.js
@@ -1,12 +1,13 @@
+import "./MetricItem.css"
+
 const MetricItem = () => {
 
-    return
-        <div>
-    <div>11/5 17:35</div>
+    return <div className="metric-item">  
+    <div className="metric-item_date">11/5 17:35</div>
     <div>Vassiliki D</div>
     <div>StockMarket Desktop App</div>
     <div>5</div>
     </div>
     
 }
-export default App;
+export default MetricItem;
