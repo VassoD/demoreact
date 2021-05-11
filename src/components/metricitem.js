49a5c180d38585d838697f75c8bd8a92ca4cd252
vassod <vassodor@gmail.com>
2021-05-11 17:44:31 +0300

    adding mockuo data

---
+++ src/components/metricitem.js
@@ -0,0 +1,12 @@
+const MetricItem = () => {
+
+    return
+        <div>
+    <div>11/5 17:35</div>
+    <div>Vassiliki D</div>
+    <div>StockMarket Desktop App</div>
+    <div>5</div>
+    </div>
+    
+}
+export default App;
