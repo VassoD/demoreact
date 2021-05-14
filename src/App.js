b08482c7bfe20cb74bf6f61dcc8f46f0bc06cf0d
vassod <vassodor@gmail.com>
2021-05-14 16:28:48 +0300

    metricName

--- src/App.js
+++ src/App.js
@@ -24,11 +24,39 @@ import './App.css';
 import MetricItem from "./components/MetricItem"
 
 function App() {
+
+
+    let metricDate=new Date(2021, 4, 12, 17, 35, 0 , 0);
+    let metricDeveloper = "Vassiliki D"
+    let project = "StockMarket Mobile App"
+    let count = 16
+
   return (
     <div>
       <h1>Hello Odyssea</h1>
-      <p>Hi from inside</p>
+      <div>
+        <input type="text" defaultValue= "username"></input>
+        <input type="password" oonclick="()=>{}"></input>
+        <p id="par1"></p>
+        <p id="par2">Hi from inside</p>
+
+      </div>
+      <MetricItem 
+        count={count}
+        project={project}
+        client="Microsoft" 
+        developer={metricDeveloper} 
+        date={metricDate}
+        />
+      <MetricItem 
+        count="4"
+        project="Backend App"
+        developer="Theo"
+        date="13/5 19:15"
+        client="Odyssea"
+      />
       <MetricItem />
+
       </div>
   );
 }
