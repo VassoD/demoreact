6adce948185f57d9f7a82d628597d7698d901801
vassod <vassodor@gmail.com>
2021-05-14 16:20:24 +0300

    MetricItem - values as variables

--- src/components/metricitem.js
+++ src/components/metricitem.js
@@ -1,13 +1,17 @@
 import "./MetricItem.css"
 
-const MetricItem = () => {
+const MetricItem = (props) => {
 
     return <div className="metric-item">  
-    <div className="metric-item_date">11/5 17:35</div>
-    <div>Vassiliki D</div>
-    <div>StockMarket Desktop App</div>
-    <div>5</div>
+    <div className="metric-item__date">{props.da.toISOString()}</div>
+    {/* metricDate.toISOString() */}
+    <div className="metric-item__metric">
+        <div>{props.developer}</div>
+        <h3>{props.project}</h3>
+        {/* Math/floor(Math.random()*100)%2===0 ? "even":"odd" */}
+        <h2>{props.client}</h2>
     </div>
-    
+    <div className="metric-item__count">{props.count}</div>
+    </div>
 }
 export default MetricItem;
