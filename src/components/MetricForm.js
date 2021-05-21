b0e6253b8ece48b61624c03fc7eac6db5cd2a93a
vassod <vassodor@gmail.com>
2021-05-21 17:26:59 +0300

    AddMetric setup UI

---
+++ src/components/MetricForm.js
@@ -0,0 +1,29 @@
+import "./MetricForm.css"
+
+const MetricForm = (props) =>{
+    return (
+        <form>
+            <div>
+                <div className="add-metric__elements">
+                    
+                </div>
+                    <label>Date</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>Developer</label>
+                    <input type="text" />
+                </div>
+                <div>
+                    <label>Project</label>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <label>Count</label>
+                    <input type="number" min="0" step="1"/>
+                </div>   
+                
+            <div>
+            
+                )
+}
